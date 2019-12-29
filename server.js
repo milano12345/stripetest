a43e3d9cd844411b422d8a3c42e136c84bde9a6e
milano12345 <dmilano857@gmail.com>
2019-12-29 15:53:44 -0600

    created env file, set up variables

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+  dotenv = require("dotenv");
+  dotenv.config();
+}
+
+const StripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const StripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+
 const express = require("express");
 const app = express();
 
