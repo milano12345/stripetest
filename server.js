27f2588ed992271531567b66ab620ad32c0fb85a
milano12345 <dmilano857@gmail.com>
2019-12-29 19:14:37 -0600

    added stripe handler

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ if (process.env.NODE_ENV !== "production") {
   dotenv.config();
 }
 
-const StripeSecretKey = process.env.STRIPE_SECRET_KEY;
-const StripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
 
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.listen(3000);
+
+app.get("/store", (req, res) => {
+  fs.readFile("items.json", function(error, data) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      res.render("store.ejs", {
+        items: JSON.parse(data),
+        stripePublicKey: stripePublicKey
+      });
+    }
+  });
+});
